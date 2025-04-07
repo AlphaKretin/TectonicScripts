62785fc8878759f3dfd3b324213f94d2d107a094
AlphaKretin <deewhydeeecks@gmail.com>
2025-04-08 08:39:31 +0930

    Add additional properties to move export

--- json_moves.py
+++ json_moves.py
@@ -27,10 +27,23 @@ def extract_move_data(move_text):
     type_match = re.search(r"^Type = (.+)", move_text, re.MULTILINE)
     type = type_match.group(1).title()
 
+    desc_match = re.search(r"^Description = (.+)", move_text, re.MULTILINE)
+    description = desc_match.group(1)
+
     bp = 0
     bp_match = re.search(r"^Power = (\d+)", move_text, re.MULTILINE)
     if bp_match:
         bp = int(bp_match.group(1))
+
+    accuracy = 0
+    acc_match = re.search(r"^Accuracy = (\d+)", move_text, re.MULTILINE)
+    if acc_match:
+        accuracy = int(acc_match.group(1))
+
+    pp = 0
+    pp_match = re.search(r"^TotalPP = (\d+)", move_text, re.MULTILINE)
+    if pp_match:
+        pp = int(pp_match.group(1))
 
     cat_match = re.search(r"^Category = (.+)", move_text, re.MULTILINE)
     category = cat_match.group(1).title()
@@ -41,8 +54,11 @@ def extract_move_data(move_text):
     move_data = {
         "id": id,
         "name": name,
+        "description": description,
         "type": type,
         "bp": bp,
+        "accuracy": accuracy,
+        "pp": pp,
         "category": category,
         "target": target,
     }
@@ -55,7 +71,7 @@ move_list = [extract_move_data(move) for move in moves]
 move_data = {move["id"]: move for move in move_list}
 
 with open("moves.json", "w", encoding="utf8") as outfile:
-    outfile.write(json.dumps(move_data, indent=4))
+    outfile.write(json.dumps(move_data))
 
 all_targets = list(set([move["target"] for move in move_list]))
 with open("movetargets.txt", "w", encoding="utf8") as outfile:
